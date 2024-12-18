2706795d49ed4b5c59d156361ffc2a42ee392dcc
제나토리 <genetory@jenatoliui-MacBookPro.local>
2024-12-18 16:58:30 +0900

    seconde

---
+++ lib/trim/video_trim_view.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class VideoTrimView extends StatefulWidget {
+  const VideoTrimView({super.key});
+
+  @override
+  _VideoTrimViewState createState() => _VideoTrimViewState();
+}
+
+class _VideoTrimViewState extends State<VideoTrimView> {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+}
+
+extension VideoTrimViewWidgetExtension on VideoTrimView {
+
+  Widget buildVideoTrimView() {
+    return Container();
+  }
+
+}
